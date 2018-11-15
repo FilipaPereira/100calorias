1626d8dd4d1080fd082306405a7da327eb7a2ef1
Jéssica Lemos <32941964+jessicalemos@users.noreply.github.com>
2018-11-15 21:03:58 +0000

    Update ScriptPovoamento.sql
    Atualização

--- ScriptPovoamento.sql
+++ ScriptPovoamento.sql
@@ -1,9 +1,9 @@
 INSERT INTO cliente
 	(Id_cliente, Nome, Data_nascimento, Rua, Codigo_postal, Telemovel, Email)
     VALUES
-    (1, 'Ana Maria', '1980-02-13', 'Rua da Capela', '4700-780', '902343333', 'ana@gmail.com'),
-    (2, 'João Moreira', '1988-04-24', 'Rua da Travessa', '4700-780', '993442211', 'joao@gmail.com'),
-    (3, 'Carlos Silva', '1990-08-30', 'Rua 25 de abril', '4700-780', '906843322', 'carlos@gmail.com'),
+    (1, 'Ana Maria', '1980-02-13', 'Rua da Capela', '4700-780', '932343333', 'ana@gmail.com'),
+    (2, 'João Moreira', '1988-04-24', 'Rua da Travessa', '4700-780', '963442211', 'joao@gmail.com'),
+    (3, 'Carlos Silva', '1990-08-30', 'Rua 25 de abril', '4700-780', '926843322', 'carlos@gmail.com'),
     (4, 'Josefina Andrade', '1999-12-12', 'Avenida Central', '4700-780', '968471443', 'josefina@gmail.com');
 
 INSERT INTO limitacao_fisica
@@ -20,15 +20,6 @@ INSERT INTO professor
     (3, 'Andre Gonçalves', '1990-03-02', 'Rua da Laje', '4700-780', '933423322', 'andreg@gmail.com'),
     (4, 'Diogo Costa', '1998-08-12', 'Rua da Cruz', '4700-780', '912345334', 'diogoCosta@gmail.com');
     
-INSERT INTO maquina
-	(Id_maquina, Tipo, Quantidade, Id_atividade)
-    VALUES
-    (1, 'Passadeira', '20', 1),
-    (2, 'Bicicleta', '25', 2),
-    (3, 'Step', '10', 3),
-    (4, 'Halteres', '10', 4),
-	(5, 'Bola de Pilates', '10', 5);
-    
 INSERT INTO atividade_fitness
 	(Id_atividade, Nr_participantes, Nome, Duracao, Sala, Id_professor)
     VALUES
@@ -37,6 +28,15 @@ INSERT INTO atividade_fitness
     (3, 10, 'Step', 45, 3, 3),
     (4, 10, 'Core', 40, 4, 4),
 	(5, 10, 'Pilates', 45, 5, 3);
+    
+INSERT INTO maquina
+	(Id_maquina, Tipo, Quantidade, Id_atividade)
+    VALUES
+    (1, 'Passadeira', '20', 1),
+    (2, 'Bicicleta', '25', 2),
+    (3, 'Step', '10', 3),
+    (4, 'Halteres', '10', 4),
+	(5, 'Bola de Pilates', '10', 5);
     
 INSERT INTO plano 
 	(Id_plano, Preco, Data_inicio, Id_professor, Id_cliente)
