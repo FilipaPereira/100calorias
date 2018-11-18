feda1051f7b6b4d8adfa0a7e6319d1f7d77a298e
Ana Ribeiro <36506313+anaribeiro142@users.noreply.github.com>
2018-11-18 10:32:59 +0000

    Update ginasio_inserir.sql

--- ginasio_inserir.sql
+++ ginasio_inserir.sql
@@ -17,3 +17,23 @@ CREATE PROCEDURE inserir_professor(IN nome VARCHAR(45), IN datanascimento DATE, 
         
 END $$  
 DELIMITER ;
+
+DROP PROCEDURE IF EXISTS inserir_cliente;
+
+DELIMITER $$
+CREATE PROCEDURE inserir_cliente(IN nome VARCHAR(45), IN datanascimento DATE, IN rua VARCHAR(45),
+		IN codigopostal VARCHAR(45), IN telemovel VARCHAR(45), IN email VARCHAR(45))
+
+		BEGIN
+		Declare erro Bool default 0;
+		declare continue handler for SQLEXCEPTION set erro = 1; 
+        start transaction;
+		INSERT INTO Cliente(Nome, Data_nascimento, Rua, Codigo_postal, Telemovel, Email)
+				VALUE (nome, datanascimento, rua, codigopostal, telemovel, email);
+        	if erro then ROLLBACK;
+    else commit;
+    
+    END IF;
+        
+END $$  
+DELIMITER ;
