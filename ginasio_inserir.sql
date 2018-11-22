cc01b1d32734d541dd4baaf013b531b58ad83b6c
Jéssica Lemos <32941964+jessicalemos@users.noreply.github.com>
2018-11-22 14:49:10 +0000

    Update ginasio_inserir.sql

--- ginasio_inserir.sql
+++ ginasio_inserir.sql
@@ -1,15 +1,22 @@
+-- Inserir um professor
 DROP PROCEDURE IF EXISTS inserir_professor;
 
 DELIMITER $$
-CREATE PROCEDURE inserir_professor(IN nome VARCHAR(45), IN datanascimento DATE, IN rua VARCHAR(45),
-		IN codigopostal VARCHAR(45), IN telemovel VARCHAR(45), IN email VARCHAR(45))
+CREATE PROCEDURE inserir_professor(IN nome VARCHAR(45), IN datanascimento DATE, IN Endereco VARCHAR(45),
+		IN telemovel CHAR(9), IN email VARCHAR(45), IN localidade VARCHAR(45))
 
 		BEGIN
 		Declare erro Bool default 0;
 		declare continue handler for SQLEXCEPTION set erro = 1; 
         start transaction;
-		INSERT INTO Professor(Nome, Data_nascimento, Rua, Codigo_postal, Telemovel, Email)
-				VALUE (nome, datanascimento, rua, codigopostal, telemovel, email);
+        set @A:=0;
+		SELECT @A:= L.Id_localidade FROM Localidade AS L where L.Nome = localidade;
+		IF(@A = 0) THEN 
+		INSERT INTO Localidade(Nome) VALUE (localidade);
+		SELECT @A:= L.Id_localidade FROM Localidade AS L where L.Nome = localidade;
+		END IF;
+		INSERT INTO Professor(Nome, Endereco, Data_nascimento, Telemovel, Email, Estado, Id_localidade)
+				VALUE (nome, endereco, datanascimento, telemovel, email, 'Ativo', @A);
         	if erro then ROLLBACK;
     else commit;
     
@@ -18,18 +25,33 @@ CREATE PROCEDURE inserir_professor(IN nome VARCHAR(45), IN datanascimento DATE, 
 END $$  
 DELIMITER ;
 
+CALL inserir_professor('Claudia Cruz', '1993-09-01', 'Rua Penedo da Marca Nº39 4715-603', '928333999', 'claudiacruz@gmail.com', 'Braga');
+
+select *
+from Professor;
+
+select *
+from Localidade;
+
+-- Inserir um cliente
 DROP PROCEDURE IF EXISTS inserir_cliente;
 
 DELIMITER $$
-CREATE PROCEDURE inserir_cliente(IN nome VARCHAR(45), IN datanascimento DATE, IN rua VARCHAR(45),
-		IN codigopostal VARCHAR(45), IN telemovel VARCHAR(45), IN email VARCHAR(45))
+CREATE PROCEDURE inserir_cliente(IN nome VARCHAR(45), IN datanascimento DATE, IN endereco VARCHAR(45),
+		IN localidade VARCHAR(45))
 
 		BEGIN
 		Declare erro Bool default 0;
 		declare continue handler for SQLEXCEPTION set erro = 1; 
         start transaction;
-		INSERT INTO Cliente(Nome, Data_nascimento, Rua, Codigo_postal, Telemovel, Email)
-				VALUE (nome, datanascimento, rua, codigopostal, telemovel, email);
+        set @A:=0;
+		SELECT @A:= L.Id_localidade FROM Localidade AS L where L.Nome = localidade;
+		IF(@A = 0) THEN 
+		INSERT INTO Localidade(Nome) VALUE (localidade);
+		SELECT @A:= L.Id_localidade FROM Localidade AS L where L.Nome = localidade;
+        END IF;
+		INSERT INTO Cliente(Nome, Data_nascimento, Endereco, Id_localidade)
+				VALUE (nome, datanascimento, endereco, @A);
         	if erro then ROLLBACK;
     else commit;
     
@@ -38,17 +60,27 @@ CREATE PROCEDURE inserir_cliente(IN nome VARCHAR(45), IN datanascimento DATE, IN
 END $$  
 DELIMITER ;
 
+CALL inserir_cliente('Nadia Santos', '2018-09-12', 'Rua Santa Clar 4830-780', 'Souto');
+
+select *
+from Cliente;
+
+-- Inserir um plano
 DROP PROCEDURE IF EXISTS inserir_plano;
 
 DELIMITER $$
-CREATE PROCEDURE inserir_plano(IN preco DOUBLE, IN datainicio DATE, IN id_professor INT, 
-									IN id_cliente INT)
+CREATE PROCEDURE inserir_plano(IN preco DOUBLE, IN datainicio DATE, IN nomeprof VARCHAR(45), 
+									IN nome VARCHAR(45))
 		BEGIN
 		Declare erro Bool default 0;
 		declare continue handler for SQLEXCEPTION set erro = 1; 
         start transaction;
-		INSERT INTO Plano (Preco, Data_inicio, Id_professor, Id_cliente)
-				VALUE (preco, datainicio, id_professor, id_cliente);
+		set @A:=0;
+		SELECT @A:= P.Id_professor FROM Professor AS P where P.Nome = nomeprof;
+		set @B:=0;
+		SELECT @B:= C.Id_cliente FROM Cliente AS C where C.Nome = nome;
+		INSERT INTO Plano(Preco, Data_inicio, Estado, Id_professor, Id_cliente)
+				VALUE (preco, datainicio, 'Ativo', @A, @B);
         	if erro then ROLLBACK;
     else commit;
     
@@ -56,3 +88,8 @@ CREATE PROCEDURE inserir_plano(IN preco DOUBLE, IN datainicio DATE, IN id_profes
         
 END $$  
 DELIMITER ;
+
+CALL inserir_plano(35,'2018-02-01','Joana Antunes', 'Ana Maria');
+
+select *
+from Plano;
