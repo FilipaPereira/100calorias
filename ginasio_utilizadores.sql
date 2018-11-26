fe6f5febd42058fb9b8c6eccc29b892c0496966f
CarlaCruz146 <carla.cruz.247@gmail.com>
2018-11-26 17:22:24 +0000

    permissoes

--- ginasio_utilizadores.sql
+++ ginasio_utilizadores.sql
@@ -21,15 +21,28 @@ GRANT EXECUTE ON PROCEDURE aumentarQt_maquina TO 'SrJoao'@'localhost';
 GRANT EXECUTE ON PROCEDURE arquivar_plano TO 'SrJoao'@'localhost';
 GRANT EXECUTE ON PROCEDURE inserir_telemovel TO 'SrJoao'@'localhost';
 GRANT EXECUTE ON PROCEDURE inserir_limitacao TO 'SrJoao'@'localhost';
+GRANT EXECUTE ON PROCEDURE inserir_atividade_plano TO 'SrJoao'@'localhost';
+GRANT EXECUTE ON PROCEDURE inserir_plano TO 'SrJoao'@'localhost';
+GRANT EXECUTE ON PROCEDURE arquivar_plano TO 'SrJoao'@'localhost';
 
 
 GRANT EXECUTE ON PROCEDURE inserir_atividade_plano TO 'Professor'@'localhost';
-GRANT EXECUTE ON PROCEDURE arquivar_plano TO 'Professor'@'localhost';
 GRANT EXECUTE ON PROCEDURE inserir_plano TO 'Professor'@'localhost';
-GRANT EXECUTE ON PROCEDURE inserir_atividade_plano TO 'Professor'@'localhost';
+GRANT EXECUTE ON PROCEDURE arquivar_plano TO 'Professor'@'localhost';
 
 
--- GRANT EXECUTE ON view_atividades TO 'Cliente' @'localhost'; 
+GRANT SELECT ON view_maquinas TO 'SrJoao' @'localhost';
+GRANT SELECT ON view_maquinas TO 'Professor' @'localhost';
+GRANT SELECT ON view_limitacao_fisica TO 'SrJoao' @'localhost';
+GRANT SELECT ON view_limitacao_fisica TO 'Professor' @'localhost';
+GRANT SELECT ON view_alunos TO 'SrJoao' @'localhost';
+GRANT SELECT ON view_alunos TO 'Professor' @'localhost';
+GRANT SELECT ON view_atividades TO 'SrJoao' @'localhost';
+GRANT SELECT ON view_atividades TO 'Professor' @'localhost';
+GRANT SELECT ON view_atividades TO 'Cliente' @'localhost';
+GRANT SELECT ON view_plano TO 'SrJoao' @'localhost';
+GRANT SELECT ON view_plano TO 'Professor' @'localhost';
+GRANT SELECT ON view_atividade_professor TO 'SrJoao' @'localhost';
 
 
 FLUSH privileges;
